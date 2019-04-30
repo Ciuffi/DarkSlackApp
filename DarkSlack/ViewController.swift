41693679381cf3c7e25fe809d0190bb89a55e017
Giulio Rossi <ciuffi9@mac.com>
2019-04-30 12:36:50 -0700

    Initial Commit

---
+++ DarkSlack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DarkSlack
+//
+//  Created by Giulio Rossi on 2019-04-30.
+//  Copyright © 2019 Giulio Rossi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
